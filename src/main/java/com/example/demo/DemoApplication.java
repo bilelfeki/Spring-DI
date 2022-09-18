a8901133eaa4199ba0cf76183a1a949ce2b8514d
bilelfeki <bilel.feki.92@gmail.com>
2022-09-18 17:14:01 +0100

    Verify your  bean instance by runig the app

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		//Dependency injection with XML
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+
+		//get the bean
+		Student student = context.getBean(Student.class);
+		System.out.println(student);
 	}
+
 
 }
