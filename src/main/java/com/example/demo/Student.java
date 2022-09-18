5e1cbceb7bdad9faffb0dea94c2e7bb3742dab04
bilelfeki <bilel.feki.92@gmail.com>
2022-09-18 17:00:11 +0100

    Add a classroom as an attribute to the student

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -4,8 +4,14 @@ public class Student {
     private int id ;
     private String name ;
 
+    private ClassRoom classRoom ;
+
     public Student(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Student(ClassRoom classRoom) {
+        this.classRoom = classRoom;
     }
 }
