52d2e564b79269d4b1f8e2b745b5141c38aa7461
bilelfeki <bilel.feki.92@gmail.com>
2022-09-18 10:37:26 +0100

    Create a class named Student.java

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Student {
+    private int id ;
+    private String name ;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
